312063044ab910c5bbdc507092939b4bb35b902c
Vinícius <vncastanheira@gmail.com>
2016-03-12 15:36:39 -0300

    Noir can walk

---
+++ Assets/MoveNavigation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveNavigation : MonoBehaviour
+{
+	public Animator animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButton (0)) {
+			NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+			agent.updateRotation = false;
+			agent.destination = GetMouse ();
+		}
+	}
+
+	Vector3 GetMouse ()
+	{
+		var target = Input.mousePosition;
+		target = Camera.main.ScreenToWorldPoint (new Vector3 (target.x, target.y, 0));
+		return target;
+	}
+}
