516619746d67fe64dd8108c0828044875422e2f7
Vinícius <vncastanheira@gmail.com>
2016-03-13 00:28:08 -0300

    * WaypointSystem2D.sln: * Grid.cs: * Playground.unity: * MoveNavigation.cs: * WaypointSystem2D.userprefs: * noir_walk.controller: * Assembly-CSharp-Editor.csproj: * noir_walk.png.meta:
    * Assembly-CSharp.csproj: Noir animates correctly

--- Assets/MoveNavigation.cs
+++ Assets/MoveNavigation.cs
@@ -3,28 +3,45 @@ using System.Collections;
 
 public class MoveNavigation : MonoBehaviour
 {
-	public Animator animator;
+    public Animator animator;
+    NavMeshAgent NavAgent;
+    SpriteRenderer Renderer;
 
-	// Use this for initialization
-	void Start ()
-	{
-		
-	}
+    Vector3 Direction = new Vector3(1.0f, 0, 0);
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastPosition = transform.position;
+        NavAgent = GetComponent<NavMeshAgent>();
+        Renderer = GetComponentInChildren<SpriteRenderer>();
+    }
 	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (Input.GetMouseButton (0)) {
-			NavMeshAgent agent = GetComponent<NavMeshAgent> ();
-			agent.updateRotation = false;
-			agent.destination = GetMouse ();
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            NavAgent.updateRotation = false;
+            NavAgent.SetDestination(GetMouse());
+        }
 
-	Vector3 GetMouse ()
-	{
-		var target = Input.mousePosition;
-		target = Camera.main.ScreenToWorldPoint (new Vector3 (target.x, target.y, 0));
-		return target;
-	}
+        // set animation
+        animator.SetBool("Walking", NavAgent.remainingDistance > 0);
+
+        // path direction
+        Direction = (transform.position - lastPosition).normalized;
+        lastPosition = transform.position;
+
+        // Flip
+        Renderer.flipX = Direction.x > 0;           
+    }
+
+    Vector3 GetMouse()
+    {
+        var target = Input.mousePosition;
+        target = Camera.main.ScreenToWorldPoint(new Vector3(target.x, target.y, 0));
+        return target;
+    }
 }
